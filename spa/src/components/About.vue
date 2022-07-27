ba9526ef4748d45b20241ea82490fcad04907013
RemonJoL <83402319+RemonJoL@users.noreply.github.com>
2022-07-27 17:12:36 +0100

    Added content to about page

--- spa/src/components/About.vue
+++ spa/src/components/About.vue
@@ -1,6 +1,27 @@
 <template>
-  <div id="about">
-    When you have a great story about how your product or service was built to change lives, share it. The "About Us" page is a great place for it to live, too. Good stories humanize your brand, providing context and meaning for your product. What’s more, good stories are sticky -- which means people are more likely to connect with them and pass them on.
+  <div>
+    <h1>About VACS</h1>
+    <h2>General</h2>
+    <p>
+      The VACS System has been developed to help inform Vault-Tec Overseers with making assessments
+      on whether or not it is potentially safe to leave the Vault,
+      if the Vault's mission allows for this.'
+    </p>
+    <h2>Disclaimer & Warnings</h2>
+    <p>
+      The VACS System only monitors weather conditions so keep in mind the following:
+      <ul>
+        <li>
+          NEVER use the VACS System as the only factor in deciding on whether or not it is safe to leave your Vault.
+        </li>
+        <li>
+          ALWAYS use the Vault-Tec Radiation Detection (VRD) System to confirm possible radiation levels local to your Vault to ensure it is safe to leave your Vault.
+        </li>
+        <li>
+          In case the VACS System is not working for your Vault, the Overseer is responsible for assigning a Vault Resident to help assess atmospheric conditions and radiation levels manually, however keep in mind this is entirely at your own risk.
+        </li>
+      </ul>
+    </p>
   </div>
 </template>
 
@@ -10,12 +31,17 @@
 </script>
 <!-- styling for the component -->
 <style>
-  #about {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+  }
+
+  li {
+    list-style: upper-alpha;
   }
 </style>
